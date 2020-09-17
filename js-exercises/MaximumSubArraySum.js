ad73862f72bf1685325121f4f1a55c98750febf6
Johann Morenogit <johanson88@gmail.com>
2020-09-17 23:41:24 +0200

    refactor: refactor function

--- js-exercises/MaximumSubArraySum.js
+++ js-exercises/MaximumSubArraySum.js
@@ -8,17 +8,22 @@ function maxSubarraySum (input, nItems) {
     }, input[0])
 }
 
+const input1 = [1,2,5,2,8,1,5];
+console.log("Input 1: ", input1);
+console.log("maxSubarraySum(input1, 4)", maxSubarraySum(input1, 4));
 
-console.log(maxSubarraySum([1,2,5,2,8,1,5], 4));
-// 17
+const input2 = [1,2,5,2,8,1,5];
+console.log("Input 2: ", input2);
+console.log("maxSubarraySum(input2, 4)", maxSubarraySum(input2, 4));
 
-console.log(maxSubarraySum([1,2,5,2,8,1,5], 2));
-// 10
+const input3 = [4,2,1,6];
+console.log("Input 3: ", input3);
+console.log("maxSubarraySum(input3, 4)", maxSubarraySum(input3, 4));
 
-console.log(maxSubarraySum([4,2,1,6], 1));
-// 6
+const input4 = [4,2,1,6,2];
+console.log("Input 4: ", input4);
+console.log("maxSubarraySum(input4, 4)", maxSubarraySum(input4, 4));
 
-console.log(maxSubarraySum([4,2,1,6,2], 4));
-// 13
-
-console.log(maxSubarraySum([], 4));
+const input5 = [];
+console.log("Input 5: ", input5);
+console.log("maxSubarraySum(input5, 4)", maxSubarraySum(input5, 4));
