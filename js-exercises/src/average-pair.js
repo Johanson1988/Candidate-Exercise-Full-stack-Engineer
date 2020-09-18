481d32b7ef43fbe040dc75e80b3a83647cf72e02
Johann Morenogit <johanson88@gmail.com>
2020-09-19 00:18:48 +0200

    fix: empty array returns null

--- js-exercises/src/average-pair.js
+++ js-exercises/src/average-pair.js
@@ -1,4 +1,5 @@
 const averagePair = (input, average) => {
+    if (!input.length) return null;
     for (let i= 0; i <= input.length; i++)
         for(let j = i + 1; j <= input.length; j++)
             if ((input[i] + input[j])/2 === average) 
