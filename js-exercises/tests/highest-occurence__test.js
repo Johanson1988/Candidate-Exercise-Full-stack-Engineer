be2ad41f775d1683f2304bb1ad6b2b67f03faa8d
Johann Morenogit <johanson88@gmail.com>
2020-09-19 01:11:01 +0200

    testing: improve testing to admit other elements in array

--- js-exercises/tests/highest-occurence__test.js
+++ js-exercises/tests/highest-occurence__test.js
@@ -6,19 +6,25 @@ describe('Highest ocurrences', function () {
   });
 
   it('highestOccurrence([2, 3, 2, 2, 2, 4, 2]) returns [2]', function () {
-    expect(highestOccurence([2, 3, 2, 2, 2, 4, 2])).toEqual([2]);
+    expect(highestOccurence([2, 3, 2, 2, 2, 4, 2]).length).toBe(1);
+    expect(highestOccurence([2, 3, 2, 2, 2, 4, 2]).includes(2)).toBe(true);
   });
 
   it('highestOccurrence([2, 3, 2, 3, 2, 3, 4]) returns [2, 3]', function () {
-    expect(highestOccurence([2, 3, 2, 3, 2, 3, 4])).toEqual([2, 3]);
+    expect(highestOccurence([2, 3, 2, 3, 2, 3, 4]).length).toBe(2);
+    expect(highestOccurence([2, 3, 2, 3, 2, 3, 4]).includes(2)).toBe(true);
+    expect(highestOccurence([2, 3, 2, 3, 2, 3, 4]).includes(3)).toBe(true);
   });
 
   it('highestOccurrence(["a", "b", "c", "a", "a", "a", "a"]) returns ["a"]', function () {
-    expect(highestOccurence(['a', 'b', 'c', 'a', 'a', 'a', 'a'])).toEqual(["a"]);
+    expect(highestOccurence(['a', 'b', 'c', 'a', 'a', 'a', 'a']).length).toBe(1);
+    expect(highestOccurence(['a', 'b', 'c', 'a', 'a', 'a', 'a']).includes("a")).toBe(true);
   });
 
   it('highestOccurrence(["a", "a", 2, 2, 2, "a", 4]) returns [2, "a"]', function () {
-    expect(highestOccurence(["a", "a", 2, 2, 2, "a", 4])).toEqual([2, "a"]);
+    expect(highestOccurence(["a", "a", 2, 2, 2, "a", 4]).length).toBe(2);
+    expect(highestOccurence(["a", "a", 2, 2, 2, "a", 4]).includes(2)).toBe(true);
+    expect(highestOccurence(["a", "a", 2, 2, 2, "a", 4]).includes("a")).toBe(true);
   });
   
 });
