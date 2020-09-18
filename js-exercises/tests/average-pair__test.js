6c8cf49e4ef142c3ea98006a24b3e0187191b5a3
Johann Morenogit <johanson88@gmail.com>
2020-09-19 00:24:17 +0200

    testing: add test & passing

--- js-exercises/tests/average-pair__test.js
+++ js-exercises/tests/average-pair__test.js
@@ -5,15 +5,19 @@ describe('Average Pair', function () {
     expect(typeof averagePair).toBe('function');
   });
 
-  it('First parameter larger', function () {
-    expect(maxOfTwoNumbers(2, 1)).toBe(2);
+  it('Empty array returns false', function () {
+    expect(averagePair([], 4)).toBe(false);
   });
 
-  it('Second parameter larger', function () {
-    expect(maxOfTwoNumbers(1, 3)).toBe(3);
+  it('averagePair([1,2,3], 2.5) returns true', function () {
+    expect(averagePair([1,2,3], 2.5)).toBe(true);
   });
 
-  it('First and Second parameter equal', function () {
-    expect(maxOfTwoNumbers(4, 4)).toBe(4);
+  it('averagePair([1,3,3,5,6,7,10,12,19], 8) returns true', function () {
+    expect(averagePair([1,3,3,5,6,7,10,12,19], 8)).toBe(true);
+  });
+
+  it('averagePair([-1,0,3,4,5,6], 4.1) returns false', function () {
+    expect(averagePair([-1,0,3,4,5,6], 4.1)).toBe(false);
   });
 });
