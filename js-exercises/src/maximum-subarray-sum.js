53a44c827425cff274d19a2e1bf2c10ba1ca9506
Johann Morenogit <johanson88@gmail.com>
2020-09-19 12:41:58 +0200

    Doc: Add function description

--- js-exercises/src/maximum-subarray-sum.js
+++ js-exercises/src/maximum-subarray-sum.js
@@ -1,3 +1,15 @@
+/**
+ * Function
+ * 
+ * Iterates the array, creating chunks of n elements and calculating the sum. if the sum is higher than stored result,
+ * new value is stored. Returns the highest value stored.
+ * 
+ * If empty array is provided, returns null.
+ * 
+ * @param {Array} input The array to analyze
+ * @param {number} nItems Number of consecutive elements
+ */
+
 function maxSubarraySum (input, nItems) {
     if (!input.length) return null;
     return input.reduce(function(accumulator, currentValue, index) {
