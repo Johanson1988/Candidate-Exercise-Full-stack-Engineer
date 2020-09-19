9e11a5187df40cee2442be6d20833b9fb9c4d382
Johann Morenogit <johanson88@gmail.com>
2020-09-19 12:58:23 +0200

    refactor: delete js-exercises from this branch

--- js-exercises/src/average-pair.js
+++
@@ -1,7 +0,0 @@
-const averagePair = (input, average) => {
-    for (let i= 0; i <= input.length; i++)
-        for(let j = i + 1; j <= input.length; j++)
-            if ((input[i] + input[j])/2 === average) 
-                return true;
-    return false;
-}
--- js-exercises/src/maximum-subarray-sum.js
+++
@@ -1,7 +0,0 @@
-function maxSubarraySum (input, nItems) {
-    if (!input.length) return null;
-    return input.reduce(function(accumulator, currentValue, index) {
-        const chuntkTotalSum = input.slice(index, index + nItems).reduce((accumulator, currentValue) => accumulator + currentValue);
-        return chuntkTotalSum > accumulator ? chuntkTotalSum : accumulator;
-    }, input[0])
-}
